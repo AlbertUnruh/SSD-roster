ccbcc3147076b2dbaaa91060a0ee5d6230b068e4
AlbertUnruh <AlbertUnruh@pm.me>
2023-12-22 17:35:08 +0100

    add basic files

---
+++ SSD_Roster/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Keenan Noack"
+__contributors__ = [__author__, ]
+__version__ = "0.0.0a"
+__license__ = "GNU AGPLv3"
+__copyright__ = f"Copyright 2023-present (c) {__author__}"
+__url__ = "https://github.com/AlbertUnruh/SSD-roster"
